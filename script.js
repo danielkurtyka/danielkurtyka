dbdea1beb14da6abdedf44feb8cc3555ce857bfd
danielkurtyka <daniel.kurtyka@gmail.com>
2026-03-12 21:47:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,27 @@
+```javascript
 document.addEventListener("DOMContentLoaded", function () {
 
 let currentLang = "pl";
 
-const langToggle = document.getElementById("langToggle");
+const toggle = document.getElementById("langToggle");
 
-langToggle.addEventListener("click", function () {
+toggle.addEventListener("click", function () {
 
 currentLang = currentLang === "pl" ? "en" : "pl";
 
-langToggle.textContent = currentLang === "pl" ? "EN" : "PL";
+toggle.textContent = currentLang === "pl" ? "EN" : "PL";
 
 document.querySelectorAll("[data-pl]").forEach(el => {
 
-el.textContent = el.getAttribute("data-" + currentLang);
+if(currentLang === "pl"){
+el.textContent = el.dataset.pl;
+} else {
+el.textContent = el.dataset.en;
+}
 
 });
 
 });
-
-
-
-function revealSections() {
-
-let reveals = document.querySelectorAll(".reveal");
-
-for (let i = 0; i < reveals.length; i++) {
-
-let windowHeight = window.innerHeight;
-
-let elementTop = reveals[i].getBoundingClientRect().top;
-
-let elementVisible = 120;
-
-if (elementTop < windowHeight - elementVisible) {
-
-reveals[i].classList.add("active");
-
-}
-
-}
-
-}
-
-
-
-window.addEventListener("scroll", revealSections);
-
-revealSections();
 
 });
 ```
-
-
-
