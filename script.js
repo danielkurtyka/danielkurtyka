b2c55ff09cbca2d5d486df04ff589fdf445c8190
danielkurtyka <daniel.kurtyka@gmail.com>
2026-03-13 14:10:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
-alert("JS działa");
+```javascript
+document.addEventListener("DOMContentLoaded", () => {
+
+let lang = "pl";
+
+const langBtn = document.getElementById("langToggle");
+
+langBtn.addEventListener("click", () => {
+
+lang = lang === "pl" ? "en" : "pl";
+
+langBtn.textContent = lang === "pl" ? "EN" : "PL";
+
+document.querySelectorAll("[data-pl]").forEach(el => {
+
+const text = el.getAttribute("data-" + lang);
+
+if (el.tagName === "INPUT" || el.tagName === "TEXTAREA") {
+el.placeholder = text;
+} else {
+el.innerText = text;
+}
+
+});
+
+});
+
+
+
+/* SCROLL ANIMATION */
+
+function reveal() {
+
+const reveals = document.querySelectorAll(".reveal");
+
+reveals.forEach(section => {
+
+const windowHeight = window.innerHeight;
+const elementTop = section.getBoundingClientRect().top;
+const visible = 120;
+
+if (elementTop < windowHeight - visible) {
+section.classList.add("active");
+}
+
+});
+
+}
+
+window.addEventListener("scroll", reveal);
+
+reveal();
+
+});
+```
