bc1c1c5ca31c70b43d12e961ca641ebba9a03978
danielkurtyka <daniel.kurtyka@gmail.com>
2026-03-19 14:26:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -54,3 +54,10 @@ const nav = document.querySelector("nav");
 menuBtn.addEventListener("click", () => {
 nav.classList.toggle("active");
 });
+
+// zamykanie menu po kliknięciu linku (mobile)
+document.querySelectorAll("nav a").forEach(link => {
+link.addEventListener("click", () => {
+nav.classList.remove("active");
+});
+});
